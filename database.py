724584f5073643919ccc586b73849c4c088d173f
Rosanna Chang <changchilun@gmail.com>
2026-02-18 13:38:07 +0800

    fix: database connection failure in docker by fixing DB_HOST logic

--- database.py
+++ database.py
@@ -8,14 +8,12 @@ from dotenv import load_dotenv
 load_dotenv()
 db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
-db_host = os.getenv("DB_HOST", "localhost")  # 如果沒設定就用 localhost
+db_host = os.getenv("DB_HOST") 
 db_port = os.getenv("DB_PORT", "5432")
 db_name = os.getenv("DB_NAME")
 
-if os.getenv("DOCKER_ENV"):
-    db_host=os.getenv("DB_HOST", "db")
-else:
-    db_host="localhost"
+db_host=os.getenv("DB_HOST", "db")
+
 SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
